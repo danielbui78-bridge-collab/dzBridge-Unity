440aa004415a204533e8a756e2cd96918eaa5f1c
danielbui78 <danielbui78@gmail.com>
2022-02-25 18:20:04 -0500

    Update version.h

--- DazStudioPlugin/version.h
+++ DazStudioPlugin/version.h
@@ -4,7 +4,7 @@
 // Version number for dzBridge-Unreal
 #define PLUGIN_MAJOR	2
 #define PLUGIN_MINOR	0
-#define PLUGIN_REV		1010
+#define PLUGIN_REV		1011
 #define PLUGIN_BUILD	0
 
 #define PLUGIN_VERSION	DZ_MAKE_VERSION( PLUGIN_MAJOR, PLUGIN_MINOR, PLUGIN_REV, PLUGIN_BUILD )
